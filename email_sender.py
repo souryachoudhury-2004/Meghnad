4ae4f6b43ceaa60d1d28242acf99ef3593c20f87
Sourya Choudhury <souryachoudhury24@gmail.com>
2023-02-14 16:01:13 +0530

    ok

--- email_sender.py
+++ email_sender.py
@@ -1,12 +1,13 @@
 import smtplib
 import ssl
-
+import os
 
 def mail(receiver, message):
     host = "smtp.gmail.com"
     port = 465
     username = "MeghnadEmailAssistant"
-    password = "xyvhgosniqcpwojp"
+    #Previously password = "xyvhgosniqcpwojp"
+    password = os.getenv("MEGHNADPASS")
     context = ssl.create_default_context()
     with smtplib.SMTP_SSL(host, port, context=context) as server:
         server.login(username, password)
